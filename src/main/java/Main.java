3aa268cd59bebc252719275e870d2a4423fdf0fc
John <johnruge@uchicago.edu>
2025-11-04 22:46:15 -0600

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,12 @@
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import commands.Commands;
 
 public class Main {
-  public static void main(String[] args){
-    final String command = args[0];
+    public static void main(String[] args) {
+        final String command = args[0];
 
-    switch (command) {
-      case "init" -> {
-        final File root = new File(".git");
-        new File(root, "objects").mkdirs();
-        new File(root, "refs").mkdirs();
-        final File head = new File(root, "HEAD");
-
-        try {
-          head.createNewFile();
-          Files.write(head.toPath(), "ref: refs/heads/main\n".getBytes());
-          System.out.println("Initialized git directory");
-        } catch (IOException e) {
-          throw new RuntimeException(e);
+        switch (command) {
+            case "init" -> Commands.init();
+            default -> System.out.println("Unknown command: " + command);
         }
-      }
-      default -> System.out.println("Unknown command: " + command);
     }
-  }
 }
